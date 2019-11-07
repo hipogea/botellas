a135ae386c0fa3f0f67c479f6a2e4bbd55e8eba5
hipogea <hipogea@hotmail.com>
2019-11-07 14:32:21 -0500

    nada

--- frontend/modules/sigi/Module.php
+++ frontend/modules/sigi/Module.php
@@ -1,16 +1,18 @@
 <?php
 
-namespace frontend\modules\people;
-
+namespace frontend\modules\sigi;
+use common\helpers\h;
+USE \yii2mod\settings\models\enumerables\SettingType;
 /**
  * people module definition class
  */
 class Module extends \yii\base\Module
 {
+    
     /**
      * {@inheritdoc}
      */
-    public $controllerNamespace = 'frontend\modules\people\controllers';
+    public $controllerNamespace = 'frontend\modules\sigi\controllers';
 
     /**
      * {@inheritdoc}
@@ -18,7 +20,14 @@ class Module extends \yii\base\Module
     public function init()
     {
         parent::init();
+        static::putSettingsModule();
 
         // custom initialization code goes here
+    }
+      private static function putSettingsModule(){
+      //  h::getIfNotPutSetting('sigi','','.jpg', SettingType::STRING_TYPE);
+       //  h::getIfNotPutSetting('sigi','urlimagesalu','http:://www.orce.uni.edu.pe/alumnos/', SettingType::STRING_TYPE);
+        // h::getIfNotPutSetting('sigi','prefiximagesalu','0060', SettingType::STRING_TYPE);
+          return true;
     }
 }
